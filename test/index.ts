0bc3163569a716ec6294ac60b1a0ab1ee0e1a368
axtk <axtkx@inbox.ru>
2023-09-15 18:29:40 +0300

    feat: add 'import/no-extraneous-dependencies' rule (#17)
    Co-authored-by: Valeriy Sidorenko <balepas@yandex-team.ru>

--- test/index.ts
+++ test/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable import/order */
 
 // eslint-disable-next-line import/no-duplicates
 import {test} from './file';
@@ -7,5 +8,11 @@ import {test2} from './file';
 import type {test4 as Test4} from './file';
 // eslint-disable-next-line import/no-duplicates
 import {test3} from './file.js';
+
 import {test4} from './file.js?mod2';
 import {test5} from './file?mod';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {parse} from 'json5';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import type {stringify} from 'json5';
